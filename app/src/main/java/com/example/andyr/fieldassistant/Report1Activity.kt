652f17f624a5175899e52e46e3d3eea72e4bed03
Andy Cook <andyrock.cook@gmail.com>
2018-03-07 13:08:39 -0500

    attempting to open camera

--- app/src/main/java/com/example/andyr/fieldassistant/Report1Activity.kt
+++ app/src/main/java/com/example/andyr/fieldassistant/Report1Activity.kt
@@ -2,17 +2,33 @@ package com.example.andyr.fieldassistant
 
 import android.content.ClipData
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.FileProvider
 import android.support.v4.content.FileProvider.getUriForFile
+import android.util.Log
 import kotlinx.android.synthetic.main.report1.*
 import java.io.File
+import android.R.attr.data
+import android.support.v4.app.NotificationCompat.getExtras
+import kotlinx.android.synthetic.main.report2.*
+
 
 class Report1Activity : AppCompatActivity() {
 
+    val APP_TAG = "FieldAssistant"
+    val CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034
+    var photoFileName = null
+    var photoFile: File? = null
+    //var report: Report = Report()
     private val TAKE_PHOTO_REQUEST_CODE = 1
+    private val REQUEST_IMAGE_CAPTURE = 1
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,13 +36,13 @@ class Report1Activity : AppCompatActivity() {
         field_camera.setOnClickListener { takePictureWithCamera() }
         field_photos.setOnClickListener { openPhotos() }
     }
-    /* //This may need to be put in Fragment
-    fun newIntent() {
-        var intent = Intent(this, ::class.java)
-        startActivity(intent)
-        finish()
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val bitmap = data!!.getExtras().get("data") as Bitmap
+        //image needs to be saved somehow
+        field_image.setImageBitmap(bitmap)
     }
-    */
 
     private fun openPhotos() {
         val intent = Intent(this, Report2Activity::class.java)
@@ -34,33 +50,40 @@ class Report1Activity : AppCompatActivity() {
     }
 
     private fun takePictureWithCamera() {
-        val intent = Intent(this, Report2Activity::class.java)
+        
+        // create Intent to take a picture and return control to the calling application
+        var intent: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, 1)
+        
+        
+        intent = Intent(this, Report2Activity::class.java)
         startActivity(intent)
+    }
 
-        /*
-        // 1
-        val captureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+    fun getPhotoFileUri(fileName: String): File? {
+        // Only continue if the SD Card is mounted
+        if (isExternalStorageAvailable()) {
+            // Get safe storage directory for photos
+            // Use `getExternalFilesDir` on Context to access package-specific directories.
+            // This way, we don't need to request external read/write runtime permissions.
+            val mediaStorageDir = File(
+                    getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG)
 
-        // 2
-        val imagePath = File(filesDir, "images")
-        val newFile = File(imagePath, "default_image.jpg")
-        if (newFile.exists()) {
-            newFile.delete()
-        } else {
-            newFile.parentFile.mkdirs()
+            // Create the storage directory if it does not exist
+            if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
+                Log.d(APP_TAG, "failed to create directory")
+            }
+
+            // Return the file target for the photo based on filename
+
+            return File(mediaStorageDir.getPath() + File.separator + fileName)
         }
-        val selectedPhotoPath = getUriForFile(this, BuildConfig.APPLICATION_ID + ".fileprovider", newFile)
+        return null
+    }
 
-        // 3
-        captureIntent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, selectedPhotoPath)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            captureIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
-        } else {
-            val clip = ClipData.newUri(contentResolver, "A photo", selectedPhotoPath)
-            captureIntent.clipData = clip
-            captureIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
-        }
-        startActivityForResult(captureIntent, TAKE_PHOTO_REQUEST_CODE)
-        */
+    // Returns true if external storage for photos is available
+    private fun isExternalStorageAvailable(): Boolean {
+        val state = Environment.getExternalStorageState()
+        return state == Environment.MEDIA_MOUNTED
     }
 }
--- app/src/main/java/com/example/andyr/fieldassistant/ReportManager.kt
+++ app/src/main/java/com/example/andyr/fieldassistant/ReportManager.kt
@@ -4,4 +4,5 @@ package com.example.andyr.fieldassistant
  * Created by andyr on 3/6/2018.
  */
 class ReportManager {
+    
 }
