ac927e30354d7851b435ddd468c62dbfbe196651
Cook <cookab1@appd.appstate.edu>
2018-03-21 14:02:05 -0400

    added code to send e-mail

--- app/src/main/java/com/example/andyr/fieldassistant/Report3Activity.kt
+++ app/src/main/java/com/example/andyr/fieldassistant/Report3Activity.kt
@@ -3,8 +3,9 @@ package com.example.andyr.fieldassistant
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.report2.*
+import android.text.format.DateFormat
 import kotlinx.android.synthetic.main.report3.*
+
 
 /**
  * Created by andyr on 2/24/2018.
@@ -13,6 +14,7 @@ class Report3Activity : AppCompatActivity() {
 
     private val TYPE_CODE = 0;
     private val DICTATE_CODE = 1;
+    private lateinit var report: Report;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,8 +27,31 @@ class Report3Activity : AppCompatActivity() {
     }
 
     private fun send() {
-        val intent = Intent(this, Report1Activity::class.java)
+        var emailList = arrayOf("andyrock.cook@gmail.com")
+        var intent: Intent = Intent(Intent.ACTION_SEND)
+        intent.type = "message/rfc822"
+        intent.putExtra(Intent.EXTRA_EMAIL, emailList)
+        intent.putExtra(Intent.EXTRA_TEXT, getReport())
+        intent = Intent.createChooser(intent, getString(R.string.send_report))
         startActivity(intent)
+
+        intent = Intent(this, Report1Activity::class.java)
+        startActivity(intent)
+    }
+
+    private fun getReport(): String {
+        
+        val dateFormat = "EEE, MMM dd"
+        val dateString = DateFormat.format(dateFormat, report.getDate()).toString()
+
+        var message = report.getMessage()
+        if (suspect == null) {
+            suspect = getString(R.string.crime_report_no_suspect)
+        } else {
+            suspect = getString(R.string.crime_report_suspect, suspect)
+        }
+
+        return getString(R.string.report, report.getLocation(), dateString, solvedString, suspect)
     }
 
     private fun keyboardInit(code: Int) {
@@ -38,5 +63,7 @@ class Report3Activity : AppCompatActivity() {
             //open keyboard then enable dictation automatically if dictate
         } else if(code == DICTATE_CODE) {
 
-        }}
+        }
+    }
+
 }
